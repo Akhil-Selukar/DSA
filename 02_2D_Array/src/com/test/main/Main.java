b6654d4c8334a345cf22989333a7a4e538c67395
Akhil Selukar <akhilselukar.work@gmail.com>
2023-08-21 23:11:01 +0530

    2D Array - array traversal

--- 02_2D_Array/src/com/test/main/Main.java
+++ 02_2D_Array/src/com/test/main/Main.java
@@ -6,12 +6,18 @@ public class Main {
 		TwoDimensionalArray tda = new TwoDimensionalArray(3, 3);
 		tda.insertValueInArray(0, 0, 10);
 		tda.insertValueInArray(0, 1, 20);
-		tda.insertValueInArray(0, 0, 30);
-		tda.insertValueInArray(1, 4, 40);
+		tda.insertValueInArray(1, 0, 30);
+		tda.insertValueInArray(1, 1, 40);
+		tda.insertValueInArray(2, 0, 50);
+		tda.insertValueInArray(2, 1, 60);
+		tda.insertValueInArray(2, 2, 70);
 		
 //		System.out.println(Arrays.deepToString(tda.arr));
 		
 		System.out.println("---------Access value at given index----------");
 		tda.accessValueAtGivenIndex(0, 1);
+		
+		System.out.println("---------Array traversal----------");
+		tda.traversse2DArray();
 	}
 }
--- 02_2D_Array/src/com/test/main/TwoDimensionalArray.java
+++ 02_2D_Array/src/com/test/main/TwoDimensionalArray.java
@@ -31,14 +31,29 @@ public class TwoDimensionalArray {
 	
 	// Access element at given index
 	public void accessValueAtGivenIndex(int rowIndex, int colIndex) {
-		System.out.println("Acccessing row "+rowIndex+" and column "+colIndex);		// O(1)
+		System.out.println("Acccessing row "+rowIndex+" and column "+colIndex);
 		try {
-			System.out.println("cell value is = "+arr[rowIndex][colIndex]);			// O(1)
+			System.out.println("cell value is = "+arr[rowIndex][colIndex]);
 		}catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("This index is not valid");							// O(1)
+			System.out.println("This index is not valid");
 		}
 	}
 	
-	// here we can clearly see that the time complexity is O(1) and as now additional 
-	// memory is involved the space complexity will also be O(1)
+	
+	// Array traversal
+	public void traversse2DArray() {
+		for(int row=0; row < arr.length; row++) {				// O(M) where M is number of rows
+			for(int col=0; col < arr[0].length; col++) {		// O(N) where N is number of columns
+				System.out.print(arr[row][col]+" ");			// O(1)
+			}
+			System.out.println();								// O(1)
+		}
+	}
+	
+	// from above code we can see that in outer loop if M is the number of rows then O(M) is the time complexity for first loop
+	// if N is the number of columns in the 2D array then O(N) will be the complexity for inner loop and the code is in the form of
+	// do inner loop for each iteration/value of outer loop. Hence it will be multiplication of complexity so overall complexity for 
+	// 2D array traversal is O(MN)
+	// as we don't need any extra space during execution of the above code hence the space complexity of the above code is O(1)
+	
 }
