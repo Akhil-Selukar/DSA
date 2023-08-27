2bd53442ebd4600c30dfe35cf0a2ce69f890490a
Akhil Selukar <akhilselukar.work@gmail.com>
2023-08-27 16:23:43 +0530

    AeeayList - traversing given ArrayList

--- 11_ArrayList_Accessing_Elements/src/com/test/main/Main.java
+++ 11_ArrayList_Accessing_Elements/src/com/test/main/Main.java
@@ -1,25 +1,43 @@
 package com.test.main;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		ArrayList<Integer> numbers = new ArrayList<Integer>();
 		numbers.add(1);
 		numbers.add(2);
 		numbers.add(3);
-		numbers.add(2,4);
-//		System.out.println(numbers);
-		System.out.println(numbers.get(2));
-		System.out.println(numbers.get(1));
+		numbers.add(4);
+		numbers.add(5);
+
+//		APPROACH 1: For loop
+		for(int i = 0; i < numbers.size(); i++) {			// O(N) as number of iterations are directly proportional to number of elements.
+			System.out.println(numbers.get(i));				// O(1)
+		}
+		// The overall time complexity for above piece of code is O(N).
+		// And as no additional memory space is required hence the space complexity is O(1).
 		
-		// We can access elements of arrayList by using .get(index) method.
-		// here we know the index and same logic as that of Array is applied and hence
-		// we can directly visit the location of given index and get the elements present there.
-		// Hence the time complexity for accessing element in ArrayList is O(1).
-		// As no additional memory space is required to access element at given index, the space complexity is also O(1)
+		System.out.println("----------------------");
+//		APPROACH 2: For each loop
+		for(Integer num : numbers) {
+			System.out.println(num);
+		}
+		// Here also we are visiting each and every element and hence the time complexity is O(N).
+		// And as no additional memory space is required the space complexity is O(1)
+		
+		System.out.println("----------------------");
+//		APPROACH 3: Iterator
+		Iterator<Integer> itr = numbers.iterator(); 		// Using iterator() method to get the iterator of arrayList.
+		while(itr.hasNext()) {								// Check for next element.
+			System.out.println(itr.next());					// Accessing the next element.
+		}
+		
+		// Here also we are visiting each and every element hence the time complexity is O(N).
+		// And as no additional space is required hence the space complexity is O(1).
 	}
 
 }
